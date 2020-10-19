e35bef1c985b139235751d2e9ec1176eaf1d80c5
Simon De Wilde <simon.dewilde2000@gmail.com>
2020-10-19 16:03:14 +0200

    Add Item repo

---
+++ TravelPackerAPI/TravelPackerAPI/Data/Repositories/ItemRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TravelPackerAPI.Models;
+using TravelPackerAPI.Models.RepositoryInterfaces;
+
+namespace TravelPackerAPI.Data.Repositories {
+	public class ItemRepository : IItemRepository {
+		public void Add(Item u) {
+			throw new NotImplementedException();
+		}
+
+		public void Delete(Item u) {
+			throw new NotImplementedException();
+		}
+
+		public IEnumerable<Item> GetAll() {
+			throw new NotImplementedException();
+		}
+
+		public Item GetById(int id) {
+			throw new NotImplementedException();
+		}
+
+		public void Update(Item u) {
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ TravelPackerAPI/TravelPackerAPI/Models/RepositoryInterfaces/IItemRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TravelPackerAPI.Models.RepositoryInterfaces {
+	public interface IItemRepository {
+
+		IEnumerable<Item> GetAll();
+
+		Item GetById(int id);
+
+		void Add(Item u);
+
+		void Delete(Item u);
+
+		void Update(Item u);
+	}
+}
