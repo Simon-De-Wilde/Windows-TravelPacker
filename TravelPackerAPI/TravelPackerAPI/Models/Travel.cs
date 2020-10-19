84db6496022bd2b7c028d9848f9b505aa2dd448d
Simon De Wilde <simon.dewilde2000@gmail.com>
2020-10-19 15:49:56 +0200

    Update Travel.cs

--- TravelPackerAPI/TravelPackerAPI/Models/Travel.cs
+++ TravelPackerAPI/TravelPackerAPI/Models/Travel.cs
@@ -1,11 +1,29 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace TravelPackerAPI.Models {
 	public class Travel {
+		public int Id { get; set; }
+		[Required]
 		public string Name { get; set; }
+		[Required]
 		public string Location { get; set; }
-		public List<Category> Categories { get; set; }
 
-		public Itinerary Itinerary { get; set; }
+		public IList<Category> Categories { get; set; }
+
+		public IList<ItineraryItem> Itineraries { get; set; }
+
+		public Travel(string name, string location) {
+			Name = name;
+			Location = location;
+
+			Categories = new List<Category>();
+
+			Itineraries = new List<ItineraryItem>();
+		}
+
+		protected Travel() {
+			// EF
+		}
 	}
 }
