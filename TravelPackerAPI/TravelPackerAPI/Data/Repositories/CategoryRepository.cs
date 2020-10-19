cab41e4d673f3fba47a451d5729daed037bfb722
Simon De Wilde <simon.dewilde2000@gmail.com>
2020-10-19 16:15:37 +0200

    Add Category repo

---
+++ TravelPackerAPI/TravelPackerAPI/Data/Repositories/CategoryRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TravelPackerAPI.Models;
+using TravelPackerAPI.Models.RepositoryInterfaces;
+
+namespace TravelPackerAPI.Data.Repositories {
+	public class CategoryRepository : ICategoryRepository {
+		public void Add(Category c) {
+			throw new NotImplementedException();
+		}
+
+		public void Delete(Category c) {
+			throw new NotImplementedException();
+		}
+
+		public IEnumerable<Category> GetAll() {
+			throw new NotImplementedException();
+		}
+
+		public Category GetById(int id) {
+			throw new NotImplementedException();
+		}
+
+		public void Update(Category c) {
+			throw new NotImplementedException();
+		}
+	}
+}
