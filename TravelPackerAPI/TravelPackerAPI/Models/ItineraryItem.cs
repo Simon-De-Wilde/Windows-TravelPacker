83559372286bad720e066ae9d6fbba6e3101d0bd
Simon De Wilde <simon.dewilde2000@gmail.com>
2020-10-19 15:49:24 +0200

    rename Itinerary to ItineraryItem

---
+++ TravelPackerAPI/TravelPackerAPI/Models/ItineraryItem.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace TravelPackerAPI.Models {
+	public class ItineraryItem : IItem {
+
+		private DateTime _start;
+
+		[Required]
+		public DateTime Start {
+			get { return _start; }
+			set {
+				if (value < DateTime.Now)
+					throw new ArgumentException("De start van een itinerary item moet in de toekomst liggen.");
+				else
+					_start = value;
+
+			}
+
+		}
+
+		[Required]
+		public TimeSpan Duration { get; set; }
+
+		[Required]
+		public override bool Done {
+			get {
+				return _done;
+			}
+			set {
+				_done = Start.AddSeconds(Duration.TotalSeconds) < DateTime.Now;
+			}
+		}
+
+		private bool _doing;
+		[Required]
+		public bool Doing {
+			get {
+				return _doing;
+			}
+			set {
+				_doing = Start < DateTime.Now;
+			}
+		}
+
+		public ItineraryItem(string title, DateTime start, TimeSpan duration) : base(title) {
+			Start = start;
+			Duration = duration;
+		}
+
+		protected ItineraryItem() {
+			// EF
+		}
+
+
+
+	}
+}
