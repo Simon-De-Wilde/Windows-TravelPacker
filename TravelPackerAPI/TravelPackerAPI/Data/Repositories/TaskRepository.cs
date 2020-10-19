ae516c17bf7871c75dee0d68232595be6f177c57
Simon De Wilde <simon.dewilde2000@gmail.com>
2020-10-19 16:09:33 +0200

    Add Task repo

---
+++ TravelPackerAPI/TravelPackerAPI/Data/Repositories/TaskRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TravelPackerAPI.Models.RepositoryInterfaces;
+
+namespace TravelPackerAPI.Data.Repositories {
+	public class TaskRepository : ITaskRepository {
+		public void Add(Models.Task u) {
+			throw new NotImplementedException();
+		}
+
+		public void Delete(Models.Task u) {
+			throw new NotImplementedException();
+		}
+
+		public IEnumerable<Models.Task> GetAll() {
+			throw new NotImplementedException();
+		}
+
+		public Models.Task GetById(int id) {
+			throw new NotImplementedException();
+		}
+
+		public void Update(Models.Task u) {
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ TravelPackerAPI/TravelPackerAPI/Models/RepositoryInterfaces/ITaskRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TravelPackerAPI.Models.RepositoryInterfaces {
+	public interface ITaskRepository {
+		IEnumerable<Task> GetAll();
+
+		Task GetById(int id);
+
+		void Add(Task u);
+
+		void Delete(Task u);
+
+		void Update(Task u);
+	}
+}
