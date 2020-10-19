fde7f9d0f10378fe1e512a83cb4798a09af4b9cf
Simon De Wilde <simon.dewilde2000@gmail.com>
2020-10-19 15:49:40 +0200

    Update IItem.cs

--- TravelPackerAPI/TravelPackerAPI/Models/IItem.cs
+++ TravelPackerAPI/TravelPackerAPI/Models/IItem.cs
@@ -1,6 +1,22 @@
-﻿namespace TravelPackerAPI.Models {
-	 public interface IItem {
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TravelPackerAPI.Models {
+	public abstract class IItem {
+		public int Id { get; set; }
+
+		[Required]
 		public string Title { get; set; }
-		public bool Done { get; set; }
+
+		protected bool _done;
+		public virtual bool Done { get { return _done; } set { _done = value; } }
+
+		public IItem(string title) {
+			Title = title;
+			Done = false;
+		}
+
+		protected IItem() {
+			// EF
+		}
 	}
 }
