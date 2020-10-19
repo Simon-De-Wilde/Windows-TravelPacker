7b6ccdd2d1a0a2040d8eb80867489247e8df6e7d
Simon De Wilde <simon.dewilde2000@gmail.com>
2020-10-19 15:50:02 +0200

    Update User.cs

--- TravelPackerAPI/TravelPackerAPI/Models/User.cs
+++ TravelPackerAPI/TravelPackerAPI/Models/User.cs
@@ -1,13 +1,33 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace TravelPackerAPI.Models {
-	public class User : IdentityUser {
-		public String FirstName { get; set; }
-		public String LastName { get; set; }
-		public List<Travel> Travels { get; set; }
+	public class User {
+		public int Id { get; set; }
+		[Required]
+		public string FirstName { get; set; }
+		[Required]
+		public string LastName { get; set; }
+		[Required]
+		public string Email { get; set; }
+
+
+		public IList<Travel> Travels { get; set; }
+
+		public User(string firstname, string lastname, string email) {
+			FirstName = firstname;
+			LastName = lastname;
+			Email = email;
+
+			Travels = new List<Travel>();
+		}
+
+		protected User() {
+			// EF
+		}
 	}
 }
