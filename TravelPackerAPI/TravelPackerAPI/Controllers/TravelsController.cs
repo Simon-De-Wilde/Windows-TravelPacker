028633e61fbd59ff7c04a380df285c19a3c75a53
Simon De Wilde <simon.dewilde2000@gmail.com>
2020-10-28 19:23:41 +0100

    Finish travelsController

--- TravelPackerAPI/TravelPackerAPI/Controllers/TravelsController.cs
+++ TravelPackerAPI/TravelPackerAPI/Controllers/TravelsController.cs
@@ -7,104 +7,119 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using TravelPackerAPI.Data;
 using TravelPackerAPI.Models;
+using TravelPackerAPI.Models.RepositoryInterfaces;
 
-namespace TravelPackerAPI.Controllers
-{
-    [Route("api/[controller]")]
-    [ApiController]
-    public class TravelsController : ControllerBase
-    {
-        private readonly TravelPackerDbContext _context;
+namespace TravelPackerAPI.Controllers {
+	[Route("api/[controller]")]
+	[ApiController]
+	public class TravelsController : ControllerBase {
+		private readonly TravelPackerDbContext _context;
+		private readonly ITravelRepository _travelRepo;
 
-        public TravelsController(TravelPackerDbContext context)
-        {
-            _context = context;
-        }
+		public TravelsController(TravelPackerDbContext context,
+		   ITravelRepository travelRepo) {
+			_context = context;
+			_travelRepo = travelRepo;
+		}
 
-        // GET: api/Travels
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<Travel>>> GetTravels()
-        {
-            return await _context.Travels.ToListAsync();
-        }
+		/// <summary>
+		/// Get all Travels
+		/// </summary>
+		/// <returns></returns>
+		// GET: api/Travels
+		[HttpGet]
+		public IEnumerable<Travel> GetTravels() {
+			return _travelRepo.GetAll();
+		}
 
-        // GET: api/Travels/5
-        [HttpGet("{id}")]
-        public async Task<ActionResult<Travel>> GetTravel(int id)
-        {
-            var travel = await _context.Travels.FindAsync(id);
+		/// <summary>
+		/// Get travel with id
+		/// </summary>
+		/// <param name="id"></param>
+		/// <returns></returns>
+		// GET: api/Travels/5
+		[HttpGet("{id}")]
+		public async Task<ActionResult<Travel>> GetTravel(int id) {
+			var travel = _travelRepo.GetById(id);
 
-            if (travel == null)
-            {
-                return NotFound();
-            }
+			if (travel == null) {
+				return NotFound();
+			}
 
-            return travel;
-        }
+			return travel;
+		}
 
-        // PUT: api/Travels/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for
-        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutTravel(int id, Travel travel)
-        {
-            if (id != travel.Id)
-            {
-                return BadRequest();
-            }
+		/// <summary>
+		/// Update an existing travel
+		/// </summary>
+		/// <param name="id"></param>
+		/// <param name="travel"></param>
+		/// <returns></returns>
+		// PUT: api/Travels/5
+		// To protect from overposting attacks, enable the specific properties you want to bind to, for
+		// more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+		[HttpPut("{id}")]
+		public async Task<IActionResult> PutTravel(int id, Travel travel) {
+			if (id != travel.Id) {
+				return BadRequest();
+			}
 
-            _context.Entry(travel).State = EntityState.Modified;
 
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!TravelExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
+			try {
 
-            return NoContent();
-        }
+				_travelRepo.Update(travel);
+				_travelRepo.SaveChanges();
 
-        // POST: api/Travels
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for
-        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
-        [HttpPost]
-        public async Task<ActionResult<Travel>> PostTravel(Travel travel)
-        {
-            _context.Travels.Add(travel);
-            await _context.SaveChangesAsync();
+			}
+			catch (DbUpdateConcurrencyException) {
+				if (!TravelExists(id)) {
+					return NotFound();
+				}
+				else {
+					throw;
+				}
+			}
 
-            return CreatedAtAction("GetTravel", new { id = travel.Id }, travel);
-        }
+			return NoContent();
+		}
 
-        // DELETE: api/Travels/5
-        [HttpDelete("{id}")]
-        public async Task<ActionResult<Travel>> DeleteTravel(int id)
-        {
-            var travel = await _context.Travels.FindAsync(id);
-            if (travel == null)
-            {
-                return NotFound();
-            }
+		/// <summary>
+		/// Add a new travel
+		/// </summary>
+		/// <param name="travel"></param>
+		/// <returns></returns>
+		// POST: api/Travels
+		// To protect from overposting attacks, enable the specific properties you want to bind to, for
+		// more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+		[HttpPost]
+		public async Task<ActionResult<Travel>> PostTravel(Travel travel) {
+			_travelRepo.Add(travel);
+			_travelRepo.SaveChanges();
 
-            _context.Travels.Remove(travel);
-            await _context.SaveChangesAsync();
+			return CreatedAtAction("GetTravel", new { id = travel.Id }, travel);
+		}
 
-            return travel;
-        }
+		/// <summary>
+		/// Delete a travel with a specific id
+		/// </summary>
+		/// <param name="id"></param>
+		/// <returns></returns>
+		// DELETE: api/Travels/5
+		[HttpDelete("{id}")]
+		public async Task<ActionResult<Travel>> DeleteTravel(int id) {
+			var travel = _travelRepo.GetById(id);
+			if (travel == null) {
+				return NotFound();
+			}
 
-        private bool TravelExists(int id)
-        {
-            return _context.Travels.Any(e => e.Id == id);
-        }
-    }
+			_travelRepo.Delete(travel);
+			_travelRepo.SaveChanges();
+
+			return travel;
+		}
+
+		private bool TravelExists(int id) {
+			return _travelRepo.GetAll().Any(e => e.Id == id);
+		}
+	}
 }
