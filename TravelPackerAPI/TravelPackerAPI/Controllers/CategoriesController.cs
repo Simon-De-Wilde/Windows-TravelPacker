4503085d1e312a0b7e4dfe68801fcde06aa345ab
Simon De Wilde <simon.dewilde2000@gmail.com>
2020-11-02 09:24:57 +0100

    Add categorie crud in API Add start of navigation

---
+++ TravelPackerAPI/TravelPackerAPI/Controllers/CategoriesController.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TravelPackerAPI.Data;
+using TravelPackerAPI.Models;
+using TravelPackerAPI.Models.RepositoryInterfaces;
+
+namespace TravelPackerAPI.Controllers {
+	[Route("api/[controller]")]
+	[ApiController]
+	[Produces("application/json")]
+	public class CategoriesController : ControllerBase {
+		private readonly TravelPackerDbContext _context;
+		private readonly ICategoryRepository _categoryRepo;
+
+		public CategoriesController(TravelPackerDbContext context,
+			ICategoryRepository categoryRepo) {
+			_context = context;
+			_categoryRepo = categoryRepo;
+		}
+
+		// GET: api/Categories
+		[HttpGet]
+		public IEnumerable<Category> GetCategories() {
+			return _categoryRepo.GetAll();
+		}
+
+		// GET: api/Categories/5
+		[HttpGet("{id}")]
+		public async Task<ActionResult<Category>> GetCategory(int id) {
+			var category = _categoryRepo.GetById(id);
+
+			if (category == null) {
+				return NotFound();
+			}
+
+			return category;
+		}
+
+		// PUT: api/Categories/5
+		// To protect from overposting attacks, enable the specific properties you want to bind to, for
+		// more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+		[HttpPut("{id}")]
+		public async Task<IActionResult> PutCategory(int id, Category category) {
+			if (id != category.Id) {
+				return BadRequest();
+			}
+
+			try {
+				_categoryRepo.Update(category);
+				_categoryRepo.SaveChanges();
+			}
+			catch (DbUpdateConcurrencyException) {
+				if (!CategoryExists(id)) {
+					return NotFound();
+				}
+				else {
+					throw;
+				}
+			}
+
+			return NoContent();
+		}
+
+		// POST: api/Categories
+		// To protect from overposting attacks, enable the specific properties you want to bind to, for
+		// more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+		[HttpPost]
+		public async Task<ActionResult<Category>> PostCategory(Category category) {
+			_categoryRepo.Add(category);
+			_categoryRepo.SaveChanges();
+
+			return CreatedAtAction("GetCategory", new { id = category.Id }, category);
+		}
+
+		// DELETE: api/Categories/5
+		[HttpDelete("{id}")]
+		public async Task<ActionResult<Category>> DeleteCategory(int id) {
+			var category = _categoryRepo.GetById(id);
+			if (category == null) {
+				return NotFound();
+			}
+
+			_categoryRepo.Delete(category);
+			_categoryRepo.SaveChanges();
+
+			return category;
+		}
+
+		private bool CategoryExists(int id) {
+			return _context.Categories.Any(e => e.Id == id);
+		}
+	}
+}
