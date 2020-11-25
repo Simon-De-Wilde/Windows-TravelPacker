12e100cc42f538562a13fbe886fb4ce11eb15394
MarbodNaassens <47554942+MarbodNaassens@users.noreply.github.com>
2020-11-25 11:11:25 +0100

    Adding extender and applying transparent background

---
+++ TravelPacker/TravelPacker/View/Travels/TravelListPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using TravelPacker.Model;
+using TravelPacker.ViewModel;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace TravelPacker.View.Travels
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class TravelListPage : Page
+    {
+        public TravelsDetailPageViewModel viewModel;
+
+        public TravelListPage()
+        {
+            this.InitializeComponent();
+            this.viewModel = new TravelsDetailPageViewModel();
+            this.DataContext = this.viewModel;
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+            Travel travel = (Travel)e.Parameter;
+
+            /*txt_title.Text = travel.Name;
+            txt_location.Text = travel.Location;
+            txt_image.Text = travel.ImageUrl;*/
+
+            viewModel.Travel = travel;
+        }
+    }
+}
---
+++ TravelPacker/TravelPacker/ViewModel/TravelsDetailPageViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TravelPacker.Model;
+
+namespace TravelPacker.ViewModel
+{
+    public class TravelsDetailPageViewModel
+    {
+        public Travel Travel { get; set; }
+
+        public TravelsDetailPageViewModel()
+        {
+        }
+    }
+}
