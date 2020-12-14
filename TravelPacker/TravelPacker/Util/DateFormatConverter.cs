9404bfba69f324d5a54c02b2bec82ae62aa197e8
Simon De Wilde <simon.dewilde2000@gmail.com>
2020-12-14 09:43:42 +0100

    format dates

---
+++ TravelPacker/TravelPacker/Util/DateFormatConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace TravelPacker.Util {
+	public class DateFormatConverter : IValueConverter {
+		public object Convert(object value, Type targetType, object parameter, string language) {
+			if (value == null)
+				return null;
+
+			DateTime dt = DateTime.Parse(value.ToString());
+			return dt.ToString("dd/MM/yyyy HH:mm");
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, string language) {
+			throw new NotImplementedException();
+		}
+	}
+}
